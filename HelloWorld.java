d7b71c63441c5155cf8ec3a5bfd5acf4a6f30f07
Patrick Coxall <mr.coxall@mths.ca>
2016-09-04 15:23:29 -0400

    Create HelloWorld.java
    Initial commit.

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+ *
+ * Created by: Mr. Coxall
+ * Created on: Feb 2016
+ * This program is the traditional Hello, World! first program.
+ *
+ ******************************************************************************/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World!");
+    }
+    
+}
